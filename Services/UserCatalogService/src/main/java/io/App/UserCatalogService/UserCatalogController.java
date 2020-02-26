0967d498ca3966cd67c613fbebbf2b34ae99e46d
MiguelMarcelino <mac.marcelino@outlook.pt>
2020-02-26 22:10:53 +0000

    Started testing with post method

--- Services/UserCatalogService/src/main/java/io/App/UserCatalogService/UserCatalogController.java
+++ Services/UserCatalogService/src/main/java/io/App/UserCatalogService/UserCatalogController.java
@@ -1,5 +1,14 @@
 package io.App.UserCatalogService;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @EnableAutoConfiguration
@@ -27,8 +40,38 @@ public class UserCatalogController {
 	}
 
 	@PostMapping(path="addUser/user", consumes= {"application/json"})
-	public void addUser(@RequestBody User user) {
+	public void addUser(@RequestBody String userJSON) {
+		System.out.println(userJSON);
+		ObjectMapper objectMapper = new ObjectMapper();
+		User user = null;
+		try {
+			user = objectMapper.readValue(userJSON, User.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		uC.addUser(user);
+	}
+
+	@RequestMapping("/test")
+	public void test() throws IOException {		
+		URL url = new URL("http://localhost:8081/addUser/user");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		OutputStream os = conn.getOutputStream();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		User user = new User(1, "Ola");
+		objectMapper.writeValue(new File("ola.txt"), user);
+		System.out.println(objectMapper.writeValueAsString(user));
+		//continue
+//		while
+//		os.write(b);
 	}
 	
 	@PostMapping("removeUser/user")
